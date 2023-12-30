67df693bf6d00aaba1d0832774e51c8ecd16c244
Prithibha48 <prithibhar48@gmail.com>
2023-12-30 14:41:39 +0530

    solved

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const bgi = './bg.jpeg';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='landing_page'>
+    <img src={bgi} alt="bg" className="background-image" />
+      <h1>Say hello to ReactJs</h1>
+      <p>You will learn frontend Framework from scratch to become a ninka developer</p>
+      <button>Awesome!</button>
     </div>
+    <div className='content'>
+      <div>
+          <img src="https://imgur.com/H2L3ohY.png" alt="img1"></img>
+          <h2>Declarative</h2>
+          <p>React makes it Painless to make interactive UIs</p>
+      </div>
+      <div>
+          <img src="https://imgur.com/IHW1mFB.png" alt="img1"></img>
+          <h2>Componenets</h2>
+          <p>Build encapsulated components that manage their state</p>
+      </div>
+      <div>
+          <img src="https://imgur.com/LtUDB9y.png" alt="img1"></img>
+          <h2>Single-Way</h2>
+          <p>A set of immutable values that are passed to components</p>
+      </div>
+      <div>
+          <img src="https://imgur.com/agN6R4Y.png" alt="img1"></img>
+          <h2>JSX</h2>
+          <p>Statically typed, designed to run on modern browsers</p>
+      </div>
+    </div>
+  </div>
   );
 }
 
